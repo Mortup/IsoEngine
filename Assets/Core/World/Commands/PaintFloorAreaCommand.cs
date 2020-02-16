ca74051de9a0aed0d3b30628eb0b1b273e69286d
Mortup <g.uribemontero@gmail.com>
2020-02-16 01:22:55 -0300

    Adds commands for painting floor tiles and areas

---
+++ Assets/Core/World/Commands/PaintFloorAreaCommand.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.mortup.iso.world.commands {
+
+    public class PaintFloorAreaCommand : IWorldCommand {
+
+        private CompositeCommand command;
+
+        public PaintFloorAreaCommand(Level level, Vector2Int start, Vector2Int end, int floorType) {
+            int xMin = Mathf.Min(start.x, end.x);
+            int xMax = Mathf.Max(start.x, end.x);
+            int yMin = Mathf.Min(start.y, end.y);
+            int yMax = Mathf.Max(start.y, end.y);
+
+            List<IWorldCommand> commands = new List<IWorldCommand>();
+
+            for (int x = xMin; x <= xMax; x++) {
+                for (int y = yMin; y <= yMax; y++) {
+                    commands.Add(new PaintFloorCommand(level, new Vector2Int(x,y), floorType));
+                }
+            }
+
+            command = new CompositeCommand(commands);
+        }
+
+        public IWorldCommand Excecute() {
+            return command.Excecute();
+        }
+    }
+
+}
---
+++ Assets/Core/World/Commands/PaintFloorCommand.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace com.mortup.iso.world.commands {
+
+    public class PaintFloorCommand : IWorldCommand {
+
+        private Level level;
+        private Vector2Int position;
+        private int floorType;
+
+        public PaintFloorCommand(Level level, Vector2Int position, int floorType) {
+            this.level = level;
+            this.position = position;
+            this.floorType = floorType;
+        }
+
+        public IWorldCommand Excecute() {
+            if (floorType == (int)FloorIndex.Empty) {
+                Debug.LogWarning("Can't use paint floor commands to build tiles.");
+                return new NullCommand();
+            }
+
+            int initialType = level.data.GetFloor(position.x, position.y);
+            if (initialType == (int)FloorIndex.Empty) {
+                return new NullCommand();
+            }
+
+            level.data.SetFloor(position.x, position.y, floorType);
+            return new PaintFloorCommand(level, position, initialType);
+        }
+    }
+
+}
