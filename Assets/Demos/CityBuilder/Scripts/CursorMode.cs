e2c598296a331cae437142653c16d8d886ed4d81
Mortup <g.uribemontero@gmail.com>
2020-03-16 22:12:28 -0300

    CityBuilder almost ready

--- Assets/Demos/CityBuilder/Scripts/CursorMode.cs
+++ Assets/Demos/CityBuilder/Scripts/CursorMode.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+
+using UnityEngine;
 using UnityEngine.EventSystems;
 
 using com.mortup.iso;
@@ -14,14 +16,20 @@ public class CursorMode : MonoBehaviour
     }
 
     [SerializeField] private Level level;
+    [SerializeField] private GameObject cursorPrefab;
+    [Header ("Sprites")]
     [SerializeField] private Sprite goodTile;
     [SerializeField] private Sprite badTile;
+    [Header("Prices")]
+    [SerializeField] private int pathMoneyPrice;
+    [SerializeField] private int houseFoodPrice;
+    [SerializeField] private int farmMoneyPrice;
 
-    private SpriteRenderer spriteRenderer;
+    private List<GameObject> cursors;
     private Mode currentMode;
 
     private void Awake() {
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        cursors = new List<GameObject>();
     }
 
     private void Start() {
@@ -29,22 +37,58 @@ public class CursorMode : MonoBehaviour
     }
 
     private void Update() {
-        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetMouseButtonDown(1)) {
-            currentMode = Mode.None;
+        DestroyCursors();
+        HandleExitCurrentMode();
+
+        if (currentMode == Mode.None)
+            return;
+        
+        Vector2Int mouseCoords = level.transformer.ScreenToTile(Input.mousePosition);
+        bool allTilesPlaceables = ShowCursor(mouseCoords);
+
+        if (currentMode == Mode.Farm) {
+
+            allTilesPlaceables = ShowCursor(mouseCoords + new Vector2Int(0, 1)) && allTilesPlaceables;
+            allTilesPlaceables = ShowCursor(mouseCoords + new Vector2Int(0, -1)) && allTilesPlaceables;
+            allTilesPlaceables = ShowCursor(mouseCoords + new Vector2Int(1, -1)) && allTilesPlaceables;
+            allTilesPlaceables = ShowCursor(mouseCoords + new Vector2Int(1, 0)) && allTilesPlaceables;
+            allTilesPlaceables = ShowCursor(mouseCoords + new Vector2Int(1, 1)) && allTilesPlaceables;
+            allTilesPlaceables = ShowCursor(mouseCoords + new Vector2Int(-1, -1)) && allTilesPlaceables;
+            allTilesPlaceables = ShowCursor(mouseCoords + new Vector2Int(-1, 0)) && allTilesPlaceables;
+            allTilesPlaceables = ShowCursor(mouseCoords + new Vector2Int(-1, 1)) && allTilesPlaceables;
         }
 
-        spriteRenderer.color = currentMode == Mode.None ? Color.clear : Color.white;
-        
-        Vector2Int tileCoords = level.transformer.ScreenToTile(Input.mousePosition);
-        transform.position = level.transformer.MouseTileRounded();
-        spriteRenderer.sortingOrder = level.transformer.TileSortingOrder(tileCoords.x, tileCoords.y) + 2;
-
-        spriteRenderer.sprite = IsTilePlaceable(tileCoords.x, tileCoords.y) ? goodTile : badTile;
-
         if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject()) {
-            level.data.SetFloor(tileCoords.x, tileCoords.y, (int)currentMode);
+            if (allTilesPlaceables) {
+                switch (currentMode) {
+                    case Mode.Path:
+                        level.data.SetFloor(mouseCoords.x, mouseCoords.y, (int)currentMode);
+                        CityBuilderManager.coins -= pathMoneyPrice;
+                        break;
+                    case Mode.House:
+                        level.data.SetFloor(mouseCoords.x, mouseCoords.y, (int)currentMode);
+                        CityBuilderManager.food -= houseFoodPrice;
+                        break;
+                    case Mode.Farm:
+                        level.data.SetFloor(mouseCoords.x+1, mouseCoords.y+1, 4);
+                        level.data.SetFloor(mouseCoords.x+1, mouseCoords.y, 4);
+                        level.data.SetFloor(mouseCoords.x+1, mouseCoords.y-1, 4);
+                        level.data.SetFloor(mouseCoords.x-1, mouseCoords.y, 4);
+                        level.data.SetFloor(mouseCoords.x-1, mouseCoords.y+1, 4);
+                        level.data.SetFloor(mouseCoords.x, mouseCoords.y+1, 4);
+                        level.data.SetFloor(mouseCoords.x, mouseCoords.y-1, 4);
+                        level.data.SetFloor(mouseCoords.x, mouseCoords.y, 4);
 
-            currentMode = Mode.None;
+                        level.data.SetFloor(mouseCoords.x-1, mouseCoords.y-1, (int)currentMode);
+                        CityBuilderManager.coins -= farmMoneyPrice;
+                        break;
+                }
+
+                currentMode = Mode.None;
+            }
+            else {
+                // Play wrong sound or something like that.
+            }
         }
     }
 
@@ -57,15 +101,74 @@ public class CursorMode : MonoBehaviour
     }
 
     private bool IsTilePlaceable(int x, int y) {
+        // Check if there's enough resources.
+        switch(currentMode) {
+            case Mode.Path:
+                if (CityBuilderManager.coins < pathMoneyPrice)
+                    return false;
+                break;
+            case Mode.House:
+                if (CityBuilderManager.food < houseFoodPrice)
+                    return false;
+                break;
+            case Mode.Farm:
+                if (CityBuilderManager.coins < farmMoneyPrice)
+                    return false;
+                break;
+        }
+
+        // Check if tile is good.
         if (level.data.IsFloorInBounds(new Vector2Int(x, y)) == false)
             return false;
 
         int tileData = level.data.GetFloor(x, y);
 
-        if (currentMode == Mode.Path) {
-            return tileData == 0;
+        if (currentMode == Mode.House) {
+            bool nextToPath = false;
+            if (level.data.IsFloorInBounds(new Vector2Int(x + 1, y))) {
+                nextToPath = nextToPath || level.data.GetFloor(x + 1, y) == 1;
+            }
+            if (level.data.IsFloorInBounds(new Vector2Int(x - 1, y))) {
+                nextToPath = nextToPath || level.data.GetFloor(x - 1, y) == 1;
+            }
+            if (level.data.IsFloorInBounds(new Vector2Int(x, y+1))) {
+                nextToPath = nextToPath || level.data.GetFloor(x, y+1) == 1;
+                if (level.data.IsFloorInBounds(new Vector2Int(x, y-1))) {
+                    nextToPath = nextToPath || level.data.GetFloor(x, y-1) == 1;
+                }
+            }
+
+            if (nextToPath == false)
+                return false;
         }
 
-        return true;
+        return tileData == 0;
+    }
+
+    private void HandleExitCurrentMode() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            currentMode = Mode.None;
+        }
+    }
+
+    private void DestroyCursors() {
+        for (int i = cursors.Count-1; i >= 0; i--) {
+            SimplePool.Despawn(cursors[i]);
+        }
+
+        cursors = new List<GameObject>();
+    }
+
+    private bool ShowCursor(Vector2Int coords) {
+        Vector3 position = level.transformer.TileToWorld(coords);
+        GameObject cursor = SimplePool.Spawn(cursorPrefab, position, Quaternion.identity);
+        cursors.Add(cursor);
+        SpriteRenderer spriteRenderer = cursor.GetComponent<SpriteRenderer>();
+
+        bool isPlaceable = IsTilePlaceable(coords.x, coords.y);
+        spriteRenderer.sprite = isPlaceable ? goodTile : badTile;
+        spriteRenderer.sortingOrder = level.transformer.TileSortingOrder(coords.x, coords.y) + 2;
+
+        return isPlaceable;
     }
 }
---
+++ Assets/Demos/CityBuilder/Scripts/Items/Farm.cs
@@ -0,0 +1,41 @@
+﻿using com.mortup.iso.observers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Farm : MonoBehaviour {
+
+    [SerializeField] private GameObject iconPrefab;
+    [SerializeField] private Transform iconSpawnPoint;
+    [SerializeField] private float foodPeriod;
+
+    private SpriteRenderer spriteRenderer;
+    private float remainingTime;
+
+    private void Awake() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start() {
+        remainingTime = Random.Range(0f, foodPeriod);
+    }
+
+    private void Update() {
+        if (remainingTime <= 0f) {
+            AddFood();
+        }
+        else {
+            remainingTime -= Time.deltaTime;
+        }
+
+        spriteRenderer.color = Color.Lerp(spriteRenderer.color, Color.white, Time.deltaTime * 2f);
+    }
+
+    private void AddFood() {
+        remainingTime = foodPeriod;
+        CityBuilderManager.food += 1;
+        Instantiate(iconPrefab, iconSpawnPoint.position, Quaternion.identity, transform);
+        spriteRenderer.color = Color.green;
+    }
+
+}
